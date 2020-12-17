abae5c03f5e10bfe6a7e202460a66298c690abed
Vincent Mackay <mackv42@gmail.com>
2020-12-17 08:33:13 -0600

    can draw walls

--- builder.js
+++ builder.js
@@ -1,10 +1,34 @@
 let hoverBox = {}
+let walls = [];
+let mouseDown = false;
 canvas.onmousemove = function(evt){
-	console.log(evt.clientX);
 	let point = clickPoint(evt.clientX, evt.clientY);
 	hoverBox = snapTo(point.x, point.y, gridWidth, gridWidth);
-	console.log(hoverBox);
+
+	if(mouseDown){
+		if(walls.filter(x => x.x == hoverBox.x && x.y == hoverBox.y).length == 0){
+			walls.push(hoverBox);
+		}
+	}
+	
 	clear();
 	drawBox(hoverBox.x, hoverBox.y, hoverBox.width, hoverBox.height);
-	// Put other drawables here
+	drawWalls();
+}
+
+function drawWalls(){
+	for(let i=0; i<walls.length; i++){
+		drawBox(walls[i].x, walls[i].y, walls[i].width, walls[i].height);
+	}
+}
+
+canvas.onmousedown = function(evt){
+	let point = clickPoint(evt.clientX, evt.clientY);
+	let newBox = snapTo(point.x, point.y, gridWidth, gridWidth);
+	mouseDown = true;
+	walls.push(newBox);
+}
+
+canvas.onmouseup = function(evt){
+	mouseDown = false;
 }
